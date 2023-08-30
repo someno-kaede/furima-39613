148816ddbe4dd4fa5ac04ad129523043df56c575
someno-kaede <rstoyo.kaede.someno@gmail.com>
2023-08-30 17:01:55 +0900

    編集機能／エラーメッセージ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @products = Product.order("created_at DESC")
@@ -29,18 +29,30 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    unless @product.user == current_user
+      redirect_to '/'
+    end
   end
 
   def update
     @product = Product.find(params[:id])
-    @product.update(product_params)
-    redirect_to root_path
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def product_params
     params.require(:product).permit(:image, :product_name, :detail, :category_id, :state_id, :burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
