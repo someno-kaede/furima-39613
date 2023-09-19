7ab3eefde59a718664b559f59c6b10bb46752886
someno-kaede <rstoyo.kaede.someno@gmail.com>
2023-09-19 11:18:41 +0900

    フォロー機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :edit_configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :reading_first_name, :reading_last_name, :birthday])
+  end
+  
+  def edit_configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :first_name, :last_name, :reading_first_name, :reading_last_name, :birthday])
   end
 end
